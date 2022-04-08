697d939e3206cdad395c05ac8f63d833c376f826
skadakar <dale.ole.morten@gmail.com>
2022-04-09 01:50:32 +0200

    Update oneline.sh

--- oneline.sh
+++ oneline.sh
@@ -41,16 +41,15 @@ wget https://raw.githubusercontent.com/skadakar/taky-itak/main/template/itak.pre
 #Adding correct IP to templates
 sed -i "s|0.0.0.0|$IP|g" *.pref
 rm /root/dp-base/certs/fts.pref
+sed -i "s|fts.pref|preference.pref|g" /root/dp-base/MANIFEST/manifest.xml
 
 #Creating itak package
 cp /root/itak.pref /root/dp-base/certs/preference.pref
-zip -j itak.zip /root/dp-base/
-
-
+zip -j -r itak.zip /root/dp-base/
 
 #Creating atak package
 cp /root/atak.pref /root/dp-base/certs/preference.pref
-zip -j atak.zip /root/dp-base/
+zip -j -r atak.zip /root/dp-base/
 #Sending files to transfer.sh where they will live for up to 14 days.
 
 itaklink=$(curl --upload-file /root/itak.zip https://transfer.sh/itak.zip)
