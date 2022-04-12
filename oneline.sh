747292e9f421ca571cc7d5f4572b382a7ce18203
skadakar <dale.ole.morten@gmail.com>
2022-04-12 09:15:25 +0200

    Update oneline.sh

--- oneline.sh
+++ oneline.sh
@@ -24,7 +24,7 @@ chown 1000 -R /root/taky-data
 
 #Downloading docker compose template 
 wget https://raw.githubusercontent.com/skadakar/taky-itak/main/docker-compose.yml
-docker pull skadakar/taky:0.8.2
+docker pull skadakar/taky:0.8.3
 
 #Starting taky servers in docker 
 docker-compose up -d && sleep 30s
